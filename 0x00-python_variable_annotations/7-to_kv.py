4d9f70f0866aeb4ee65658d83bb5d990814a7bbd
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-04 17:48:27 +0100

    Added proper docstrings for Task 0-102

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,20 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''Converts a key and its value to a tuple of the key and
-    the square of its value.
+    '''
+    Converts a key and its value to a tuple of the key and the square of its value.
+
+    Args:
+        k (str): The key to be converted.
+        v (Union[int, float]): The value to be squared.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the key and the square of its value.
+
+    Example:
+        >>> to_kv('x', 3)
+        ('x', 9.0)
+        >>> to_kv('y', 4.5)
+        ('y', 20.25)
     '''
     return (k, float(v**2))
