e6e70f32fb3ad6b5bbb0d78f98421049eda43533
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-04 17:48:30 +0100

    Added proper docstrings for Task 0-102

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,21 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    '''Creates a multiplier function.
+    '''
+    Creates a multiplier function that multiplies its input by a given factor.
+
+    Args:
+        multiplier (float): The factor by which the input will be multiplied.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float as input and returns the product of the input and the multiplier.
+
+    Example:
+        >>> double = make_multiplier(2.0)
+        >>> double(5.0)
+        10.0
+        >>> triple = make_multiplier(3.0)
+        >>> triple(5.0)
+        15.0
     '''
     return lambda x: x * multiplier
