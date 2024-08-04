f8f78ca7cafe78af5d7ec7793f384bab6344c42c
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-04 17:48:17 +0100

    Added proper docstrings for Task 0-102

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,6 +4,17 @@
 
 
 def to_str(n: float) -> str:
-    '''Converts a floating-point number to a string.
+    '''
+    Converts a floating-point number to a string.
+
+    Args:
+        n (float): The floating-point number to be converted.
+
+    Returns:
+        str: The string representation of the input number.
+
+    Example:
+        >>> to_str(3.14)
+        '3.14'
     '''
     return str(n)
