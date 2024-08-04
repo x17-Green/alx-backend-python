c46deb66074fd9b1ebc18da9b247d281a8e4002c
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-04 17:48:15 +0100

    Added proper docstrings for Task 0-102

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,6 +4,23 @@
 
 
 def floor(n: float) -> int:
-    '''Computes the floor of a floating-point number.
+    '''
+    Computes the floor of a floating-point number.
+
+    The floor of a number is the largest integer less than or equal to the number.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        int: The floor of the input number.
+
+    Examples:
+        >>> floor(3.7)
+        3
+        >>> floor(-3.7)
+        -4
+        >>> floor(3.0)
+        3
     '''
     return int(n)
