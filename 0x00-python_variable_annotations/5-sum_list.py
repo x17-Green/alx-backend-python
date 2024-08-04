f121bb2d97b8dca3fb45a7c02941dd11081a4aa8
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-04 17:48:22 +0100

    Added proper docstrings for Task 0-102

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,6 +5,17 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''Computes the sum of a list of floating-point numbers.
+    '''
+    Computes the sum of a list of floating-point numbers.
+
+    Args:
+        input_list (List[float]): A list of floating-point numbers.
+
+    Returns:
+        float: The sum of the input list.
+
+    Example:
+        >>> sum_list([1.0, 2.0, 3.0])
+        6.0
     '''
     return float(sum(input_list))
